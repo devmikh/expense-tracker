f0ccd975bf15d4d874d1df741bf526450c708fc2
Mikhail Serebriakov <mishacyb@gmail.com>
2019-10-22 10:53:54 -0700

    Added new expense functionality

--- routes/expenses.js
+++ routes/expenses.js
@@ -24,9 +24,44 @@ router.get("/", auth, async (req, res) => {
 // @route     POST api/expenses
 // @desc      Add new expense
 // @access    Private
-router.post("/", (req, res) => {
-  res.send("Add expense entry");
-});
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("amount", "Please enter an amount")
+        .not()
+        .isEmpty(),
+      check("category", "Please select a category")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { amount, description, category, date } = req.body;
+
+    try {
+      const newExpense = new Expense({
+        amount,
+        description,
+        category,
+        date,
+        user: req.user.id
+      });
+
+      const expense = await newExpense.save();
+      res.json(expense);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 // @route     PUT api/expenses/:id
 // @desc      Update expense
