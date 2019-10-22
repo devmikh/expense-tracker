318fed3380efbbeb8a21159c0797c2cb35e958df
Mikhail Serebriakov <mishacyb@gmail.com>
2019-10-22 10:27:52 -0700

    Added expense model

--- routes/expenses.js
+++ routes/expenses.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
+const Expense = require("../models/Expense");
 
 // @route     GET api/expenses
 // @desc      Get all expenses of a user
 // @access    Private
-router.get("/", (req, res) => {
-  res.send("Get all expenses");
+router.get("/", auth, async (req, res) => {
+  try {
+    const expenses = await Expense.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    res.json(expenses);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route     POST api/expenses
